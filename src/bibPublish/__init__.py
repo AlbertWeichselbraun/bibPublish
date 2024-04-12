c24a9f3c0f90f234938fb60cae1ea6f485afdc63
Albert Weichselbraun <albert@weichselbraun.net>
2024-04-12 10:58:18 +0200

    chg: cleanup for pipx package.

--- src/bibPublish/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-bibPublish
-(C)opyrights 2020 Albert Weichselbraun
-'''
-
-__version__ = '0.1'
-__author__ = 'Albert Weichselbraun'
-__author_email__ = 'albert.weichselbraun@fhgr.ch'
-__license__ = 'GPL3'
